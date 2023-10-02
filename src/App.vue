8a9fd1c163df586a6efc43aa5c77e0bdaaf8f5d2
Sebastian Ospina <grinmord@gmail.com>
2023-10-02 14:10:37 +0100

    super opera hecho

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="menu" app temporary>
+      <v-list>
+        <v-list-tile @click="seleccionar('home')">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ 'Inicio' }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('perfil')">
+          <v-list-tile-action>
+            <v-icon>account_circle</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ 'perfil' }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('registro')">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ 'registro' }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('login')">
+          <v-list-tile-action>
+            <v-icon>arrow_forward</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ 'ingresar' }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar color="primary" dark app>
-      <v-toolbar-title class="headline text-uppercase">
+      <v-toolbar-side-icon @click="menu = !menu"></v-toolbar-side-icon>
+      <v-toolbar-title @click="componenteActual = 'home'" class="headline logo">
         <span>{{ titulo }}</span>
       </v-toolbar-title>      
     </v-toolbar>
 
     <v-content>
-      <home />
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+          <component :is="componenteActual"></component>
+        </v-slide-y-transition>
+      </v-container>
     </v-content>
+
+    <v-footer color="primary" dark>
+      <v-layout justify-center>
+        <span>Curso Vue.js y Firebase - Jorge Bustamante</span>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
 import Home from '@/views/Home.vue'
+import Registro from '@/views/usuario/Registro.vue'
+import Login from '@/views/usuario/Login.vue'
+import Perfil from '@/views/usuario/Perfil.vue'
 
 export default {
-  components: { Home },
+  components: { Home, Registro, Login, Perfil },
   name: 'App',
   data () {
     return {
-      titulo: 'Súper Ópera'
+      titulo: 'Súper Ópera',
+      componenteActual: 'home',
+      menu: false
+    }
+  },
+  methods: {
+    seleccionar(nombre) {
+      this.componenteActual = nombre
+      this.menu = false
     }
   }
 }
 </script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Great+Vibes');
+
+.logo {
+  font-family: 'Great Vibes', cursive !important;
+  cursor: pointer;
+}
+
+</style>
